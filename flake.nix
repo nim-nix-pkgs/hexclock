23172ded052f3e6e8f61359cd25cb3564d9dd91d
hugosenari <hugosenari@gmail.com>
2022-03-12 02:11:06 -0300

    chore: re index hexclock

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Hex clock made in SDL and Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."hexclock-master".url = "path:./master";
+  inputs."hexclock-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."hexclock-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
